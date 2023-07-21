928b386f84b1eb6051eebd5183f95a2adf5eeaa3
jgunn <j.s.gunn22@gmail.com>
2023-07-21 13:55:21 -0700

    adds listeners to nav bar

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 $(function () {
   let body = $("body");
+  let nav = $("nav");
   let root = $("#root");
 
+  // nav bar listeners
+  let returnToLandingBtn = nav.children().eq(0);
+  let searchHistoryBtn = nav.children().eq(1).children().eq(0);
+  let iveReviewedBtn = nav.children().eq(1).children().eq(1);
+
+  returnToLandingBtn.on("click", function () {
+    console.log("this renders the landing page");
+    landingPage();
+  });
+
+  searchHistoryBtn.on("click", function () {
+    console.log("this renders my search history");
+  });
+
+  iveReviewedBtn.on("click", function () {
+    console.log("this renders games ive reviewed");
+  });
+
   function landingPage() {
+    root.text(""); // clears root before rendering.
+    // sets background image and opacity
     root.css({
       backgroundImage:
         "linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url(https://images8.alphacoders.com/954/thumb-1920-954028.jpg)",
@@ -28,6 +49,10 @@ $(function () {
     });
     greetingDiv.append(searchBtn);
     searchBtn.text("Show me what you've got!");
+
+    searchBtn.on("click", function () {
+      console.log("this will search");
+    });
   }
 
   landingPage();
