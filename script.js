4e4dd31ce11afbf0eb2fd5595a372a5306908f59
jgunn <j.s.gunn22@gmail.com>
2023-07-21 15:29:23 -0700

    adds the historical searches page. WIP

--- script.js
+++ script.js
@@ -15,12 +15,14 @@ $(function () {
 
   searchHistoryBtn.on("click", function () {
     console.log("this renders my search history");
+    searchHistory();
   });
 
   iveReviewedBtn.on("click", function () {
     console.log("this renders games ive reviewed");
   });
 
+  // renders landing page
   function landingPage() {
     root.text(""); // clears root before rendering.
     // sets background image and opacity
@@ -55,5 +57,91 @@ $(function () {
     });
   }
 
-  landingPage();
+  // listener for history cards - temporily prints game title in console - will eventually render that games info page.
+  root.on("click", ".historyCard", function () {
+    let title = $(this).children().eq(1).text();
+    console.log(title);
+  });
+
+  // renders the Search history (UI only) when nav link is clicked
+  function searchHistory() {
+    // resets root
+    root.text("");
+    root.css("background", "none");
+
+    // this array is temporary for the sake of building the components.  It will need to be updated to get search history from localStorage
+    let tempArray = [
+      {
+        name: "Grand Theft Auto V",
+        image:
+          "https://media.rawg.io/media/games/456/456dea5e1c7e3cd07060c14e96612001.jpg",
+        rating: "4.47",
+        release: "2013-09-17",
+        price: "$10.99",
+      },
+      {
+        name: "Grand Theft Auto V",
+        image:
+          "https://media.rawg.io/media/games/456/456dea5e1c7e3cd07060c14e96612001.jpg",
+        rating: "4.47",
+        release: "2013-09-17",
+        price: "$10.99",
+      },
+      {
+        name: "Grand Theft Auto V",
+        image:
+          "https://media.rawg.io/media/games/456/456dea5e1c7e3cd07060c14e96612001.jpg",
+        rating: "4.47",
+        release: "2013-09-17",
+        price: "$10.99",
+      },
+      {
+        name: "Grand Theft Auto V",
+        image:
+          "https://media.rawg.io/media/games/456/456dea5e1c7e3cd07060c14e96612001.jpg",
+        rating: "4.47",
+        release: "2013-09-17",
+        price: "$10.99",
+      },
+      {
+        name: "Grand Theft Auto V",
+        image:
+          "https://media.rawg.io/media/games/456/456dea5e1c7e3cd07060c14e96612001.jpg",
+        rating: "4.47",
+        release: "2013-09-17",
+        price: "$10.99",
+      },
+    ];
+
+    let historyCardDiv = $("<div>");
+    root.append(historyCardDiv);
+    historyCardDiv.addClass("historyCardDiv");
+
+    // creates a historyCard for every item stored in the array
+    $.each(tempArray, function (i) {
+      let card = $("<div>");
+      let img = $("<img>");
+      let title = $("<h3>");
+      let release = $("<p>");
+      let ratingDiv = $("<div>");
+      let ratingLabel = $('<p class="small-body">Avg. score</p>');
+      let rating = $("<h2>");
+
+      historyCardDiv.append(card);
+      card.addClass("historyCard");
+      card.append(img);
+      img.attr("src", tempArray[i].image);
+      card.append(title);
+      title.text(tempArray[i].name);
+      card.append(release);
+      release.addClass("small-body release");
+      release.text(tempArray[i].release);
+      card.append(ratingDiv);
+      ratingDiv.append(ratingLabel);
+      ratingDiv.append(rating);
+      rating.text(tempArray[i].rating);
+    });
+  }
+
+  landingPage(); // renders the landing page on load
 });
